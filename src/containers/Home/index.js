70e3bd8645efcb64168b18231fb716689c5c2ec9
Peter Rupa <megatricycle@gmail.com>
2017-05-21 17:32:24 +0800

    Added image uploader in products

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -6,7 +6,7 @@ import {
     Header,
     Button,
     List,
-    Image,
+    Image as SemanticImage,
     Form,
     Grid,
     Modal
@@ -16,17 +16,79 @@ import ProductActions from '../../redux/product';
 import './style.css';
 
 class Home extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            image: null
+        };
+    }
+
+    openFileUploader = () => {
+        const { uploader } = this.refs;
+
+        uploader.click();
+    };
+
+    handleChangeImagePreview = e => {
+        const image = e.target.files[0];
+
+        if (image) {
+            const reader = new FileReader();
+
+            reader.onload = e => {
+                const { result } = e.target;
+                const img = new Image();
+
+                img.src = result;
+                img.onload = () => {
+                    this.setState({
+                        image: result
+                    });
+                };
+            };
+
+            reader.readAsDataURL(image);
+        }
+    };
+
+    submitAddProductForm = () => {
+        const addProductForm = document.getElementById('add-product-form');
+
+        addProductForm.dispatchEvent(new Event('submit'));
+    };
+
+    handleAddProduct = e => {
+        e.preventDefault();
+
+        // const name = e.target.name.value;
+        // const descriptionSummary = e.target.descriptionSummary.value;
+        // const descriptionDetail = e.target.descriptionDetail.value;
+
+        // @TODO: api call here
+    };
+
     componentDidMount() {
         const { userId } = this.props.user.user;
         const { requestProducts } = this.props;
+        const { uploader } = this.refs;
+        const { handleChangeImagePreview } = this;
 
         requestProducts(userId);
+
+        uploader.onchange = handleChangeImagePreview;
     }
 
     render() {
         const { push } = this.props.history;
         const { products } = this.props.product;
         const { user } = this.props.user;
+        const { image } = this.state;
+        const {
+            openFileUploader,
+            handleAddProduct,
+            submitAddProductForm
+        } = this;
 
         const addProductModal = (
             <Modal
@@ -36,44 +98,55 @@ class Home extends Component {
             >
                 <Modal.Header>Add a Product</Modal.Header>
                 <Modal.Content>
-                    <Form>
+                    <Form id="add-product-form" onSubmit={handleAddProduct}>
                         <Grid columns={2}>
                             <Grid.Column>
                                 <Form.Input
                                     label="Name"
                                     placeholder="Product name"
+                                    name="name"
                                 />
                             </Grid.Column>
                             <Grid.Column className="image-uploader-container">
-                                <a href="#" className="image-uploader">
-                                    <FontAwesome
-                                        name="plus-square-o"
-                                        size="3x"
-                                    />
-                                    <span className="label">
-                                        Add Image
-                                    </span>
+                                <a href="#" onClick={openFileUploader}>
+                                    {image
+                                        ? <div
+                                              className="image-uploader-preview"
+                                              style={{
+                                                  backgroundImage: `url("${image}")`
+                                              }}
+                                          />
+                                        : <div className="image-uploader">
+                                              <FontAwesome
+                                                  name="plus-square-o"
+                                                  size="3x"
+                                              />
+                                              <span className="label">
+                                                  Add Image
+                                              </span>
+                                          </div>}
                                 </a>
-                                <input
-                                    type="file"
-                                    ref="uploader"
-                                    style={{ display: 'none' }}
-                                    accept="image/*"
-                                />
                             </Grid.Column>
                         </Grid>
                         <Form.TextArea
                             label="Summary"
                             placeholder="Enter a short description of your product here"
+                            name="descriptionSummary"
                         />
                         <Form.TextArea
                             label="Detail"
                             placeholder="Enter the details of your product here"
+                            name="descriptionDetail"
                         />
                     </Form>
                 </Modal.Content>
                 <Modal.Actions>
-                    <Button content="Create" icon="plus" primary />
+                    <Button
+                        content="Create"
+                        icon="plus"
+                        primary
+                        onClick={submitAddProductForm}
+                    />
                 </Modal.Actions>
             </Modal>
         );
@@ -86,6 +159,12 @@ class Home extends Component {
                     </Header>
 
                     {addProductModal}
+                    <input
+                        type="file"
+                        ref="uploader"
+                        style={{ display: 'none' }}
+                        accept="image/*"
+                    />
 
                     <List selection verticalAlign="middle" divided size="big">
                         {products.map(product => (
@@ -95,7 +174,7 @@ class Home extends Component {
                                     push(`/home/products/${product.id}`);
                                 }}
                             >
-                                <Image
+                                <SemanticImage
                                     avatar
                                     src="http://placehold.it/150x150"
                                 />
